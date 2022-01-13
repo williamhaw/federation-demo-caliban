3fe00b64a490069cccbc38456613b3006a995c74
William Haw <me@williamhaw.com>
2022-01-13 18:41:54 +0800

    Fix fragment error in ReviewsServer by implementing EnitityResolvers for User and Product

--- reviews/src/main/scala/com/williamhaw/gql_caliban/reviews/ReviewsServer.scala
+++ reviews/src/main/scala/com/williamhaw/gql_caliban/reviews/ReviewsServer.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import caliban.GraphQL.graphQL
 import caliban.federation._
+import caliban.schema.ArgBuilder
 import caliban.{AkkaHttpAdapter, RootResolver}
 import sttp.tapir.json.play._
 import zio.query.ZQuery
@@ -26,56 +27,97 @@ object ReviewsServer extends App {
 
   @GQLKey("id")
   @GQLExtend
-  case class User(@GQLExternal id: UUID, @GQLExternal username: Option[String] = None, reviews: Seq[Review] = Seq.empty)
+  case class User(@GQLExternal id: UUID, @GQLExternal username: Option[String], reviews: Seq[Review])
 
   @GQLKey("upc")
   @GQLExtend
   case class Product(@GQLExternal upc: String, reviews: Seq[Review])
 
+  case class ReviewInternal(
+      id: UUID,
+      body: Option[String],
+      author: Option[UserInternal],
+      product: Option[ProductInternal]
+  )
+  case class UserInternal(id: UUID)
+  case class ProductInternal(upc: String)
+
   val users = Seq(
-    User(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1"), Some("@ada"), Seq.empty),
-    User(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"), Some("@complete"), Seq.empty)
+    UserInternal(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1")),
+    UserInternal(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"))
   )
 
   val reviews = Seq(
-    Review(
+    ReviewInternal(
       UUID.fromString("a6c43de7-5eab-4c83-97bf-66b44cf533e8"),
       Some("Love it!"),
-      Some(User(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1"))),
-      Some(Product("1", Seq.empty))
+      Some(users.head),
+      Some(ProductInternal("1"))
     ),
-    Review(
+    ReviewInternal(
       UUID.fromString("e4e8f4af-0161-4dad-8d2c-6c713381503f"),
       Some("Too expensive."),
-      Some(User(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1"))),
-      Some(Product("2", Seq.empty))
+      Some(users.head),
+      Some(ProductInternal("2"))
     ),
-    Review(
+    ReviewInternal(
       UUID.fromString("add48364-a071-49a9-9b67-db95a22ba99a"),
       Some("Could be better."),
-      Some(User(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"))),
-      Some(Product("3", Seq.empty))
+      Some(users(1)),
+      Some(ProductInternal("3"))
     ),
-    Review(
+    ReviewInternal(
       UUID.fromString("aa5381ef-1531-4d6a-af09-6c8bd0c49843"),
       Some("Prefer something else."),
-      Some(User(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"))),
-      Some(Product("1", Seq.empty))
+      Some(users(1)),
+      Some(ProductInternal("1"))
     )
   )
 
-  def getReview(args: ReviewArgs) = reviews.find(_.id == args.id)
-
   case class ReviewArgs(id: UUID)
-  case class UserArgs(authorID: UUID)
+  case class ReviewResolvedArgs(id: UUID, author: User, product: Product)
+  case class UserArgs(id: UUID)
   case class ProductArgs(upc: String)
+
+  implicit lazy val userArgBuilder: ArgBuilder[User]       = ArgBuilder.gen[User]
+  implicit lazy val productArgBuilder: ArgBuilder[Product] = ArgBuilder.gen[Product]
 
   case class Queries(review: ReviewArgs => Option[Review])
 
-  val queries: Queries = Queries(args => getReview(args))
-
-  val api = graphQL(RootResolver(queries)) @@ federated(
-    EntityResolver.from[ReviewArgs](args => ZQuery.fromEffect(UIO(reviews.find(_.id == args.id))))
+  val api = graphQL(RootResolver()) @@ federated(
+    EntityResolver.from[ReviewResolvedArgs](args =>
+      ZQuery.fromEffect(UIO(reviews.find(_.id == args.id).map { r =>
+        Review(r.id, r.body, Some(args.author), Some(args.product))
+      }))
+    ),
+    EntityResolver.from[UserArgs](args =>
+      ZQuery.fromEffect(
+        UIO.succeed(
+          users.find(_.id == args.id).map { u =>
+            User(
+              u.id,
+              None,
+              reviews.filter(_.author.map(_.id == u.id).isDefined).map(r => Review(r.id, r.body, None, None))
+            )
+          }
+        )
+      )
+    ),
+    EntityResolver.from[ProductArgs](args =>
+      ZQuery.fromEffect(
+        UIO.succeed(
+          if (reviews.exists(_.product.map(_.upc == args.upc).isDefined))
+            Some(
+              Product(
+                args.upc,
+                reviews.filter(_.product.map(_.upc == args.upc).isDefined).map(r => Review(r.id, r.body, None, None))
+              )
+            )
+          else
+            None
+        )
+      )
+    )
   )
 
   println(api.render)
