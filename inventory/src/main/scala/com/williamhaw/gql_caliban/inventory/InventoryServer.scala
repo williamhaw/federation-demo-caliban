4459e50be3cafc73ee0f78089345245ac6cf845d
William Haw <me@williamhaw.com>
2022-01-13 16:49:41 +0800

    Refactor to use ProductInternal in InventoryServer and create extended Product type on demand

--- inventory/src/main/scala/com/williamhaw/gql_caliban/inventory/InventoryServer.scala
+++ inventory/src/main/scala/com/williamhaw/gql_caliban/inventory/InventoryServer.scala
@@ -24,26 +24,38 @@ object InventoryServer extends App {
       @GQLRequires("price weight") shippingEstimate: Option[Int]
   )
 
+  case class ProductInternal(upc: String, inStock: Boolean)
+
   val inventory = Seq(
-    Product("1", None, None, inStock = Some(true), None),
-    Product("2", None, None, inStock = Some(false), None),
-    Product("3", None, None, inStock = Some(true), None)
+    ProductInternal("1", inStock = true),
+    ProductInternal("2", inStock = false),
+    ProductInternal("3", inStock = true)
   )
 
   case class ProductArgs(upc: String, price: Option[Int], weight: Option[Int])
 
   val api = graphQL(RootResolver()) @@ federated(
     EntityResolver.from[ProductArgs](args =>
-      ZQuery.fromEffect(UIO(inventory.find(_.upc == args.upc)
-        .map { i =>
-        // free for expensive items
-        if (args.price.forall(_ > 1000))
-          i.copy(shippingEstimate = Some(0))
-        else
-          // estimate is based on weight
-          i.copy(shippingEstimate = Some((args.weight.getOrElse(0) * 0.5).toInt))
-      }
-      ))
+      ZQuery.fromEffect(
+        UIO(
+          inventory
+            .find(_.upc == args.upc)
+            .map { p =>
+              // free for expensive items
+              if (args.price.forall(_ > 1000)) {
+                Product(p.upc, args.weight, args.price, inStock = Some(p.inStock), shippingEstimate = Some(0))
+              } else
+                // estimate is based on weight
+                Product(
+                  p.upc,
+                  args.weight,
+                  args.price,
+                  inStock = Some(p.inStock),
+                  shippingEstimate = Some((args.weight.getOrElse(0) * 0.5).toInt)
+                )
+            }
+        )
+      )
     )
   )
 
