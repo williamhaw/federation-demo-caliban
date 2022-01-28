1ed1538c5de87fd33eab70d7942219def58b3fa1
William Haw <me@williamhaw.com>
2022-01-28 12:26:14 +0800

    Add mutations to AccountsApi and ReviewsApi

--- accounts/src/main/scala/com/williamhaw/gql_caliban/accounts/AccountsApi.scala
+++ accounts/src/main/scala/com/williamhaw/gql_caliban/accounts/AccountsApi.scala
@@ -8,27 +8,32 @@ import zio.UIO
 import zio.query.ZQuery
 
 import java.util.UUID
+import java.util.concurrent.atomic.AtomicReference
 
 object AccountsApi {
 
   @GQLKey("id")
   case class User(id: UUID, name: Option[String], username: Option[String])
 
-  val users = Seq(
+  val users = new AtomicReference(Seq(
     User(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1"), Some("Ada Lovelace"), Some("@ada")),
     User(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"), Some("Alan Turing"), Some("@complete"))
-  )
+  ))
 
-  def me: User = users.head
+  def me: User = users.get().head
+  def getUser(args: UserArgs): Option[User] = users.get().find(_.id == args.id)
+  def addUser(newUser: User): Unit = users.set(users.get() :+ newUser)
 
   case class UserArgs(id: UUID)
 
-  case class Queries(me: () => User)
+  case class Queries(me: () => User, getUser: UserArgs => Option[User])
+  case class Mutations(addUser: User => Unit)
 
-  val queries: Queries = Queries(() => me)
+  val queries: Queries = Queries(() => me, getUser)
+  val mutations: Mutations = Mutations(addUser)
 
-  val api = graphQL(RootResolver(queries)) @@ federated(
-    EntityResolver.from[UserArgs](args => ZQuery.fromEffect(UIO(users.find(_.id == args.id))))
+  val api = graphQL(RootResolver(queries, mutations)) @@ federated(
+    EntityResolver.from[UserArgs](args => ZQuery.fromEffect(UIO(users.get().find(_.id == args.id))))
   ) @@ ApolloFederatedTracing.wrapper
 
 }
--- reviews/src/main/scala/com/williamhaw/gql_caliban/reviews/ReviewsApi.scala
+++ reviews/src/main/scala/com/williamhaw/gql_caliban/reviews/ReviewsApi.scala
@@ -7,6 +7,7 @@ import caliban.federation.tracing.ApolloFederatedTracing
 import zio.query.ZQuery
 
 import java.util.UUID
+import java.util.concurrent.atomic.AtomicReference
 
 object ReviewsApi {
 
@@ -40,43 +41,47 @@ object ReviewsApi {
   case class UserInternal(id: UUID, username: Option[String])
   case class ProductInternal(upc: String)
 
-  val users = Seq(
-    UserInternal(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1"), Some("@ada")),
-    UserInternal(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"), Some("@complete"))
-  )
-
-  val reviews = Seq(
-    ReviewInternal(
-      UUID.fromString("a6c43de7-5eab-4c83-97bf-66b44cf533e8"),
-      Some("Love it!"),
-      Some(users.head.id),
-      Some(ProductInternal("1"))
-    ),
-    ReviewInternal(
-      UUID.fromString("e4e8f4af-0161-4dad-8d2c-6c713381503f"),
-      Some("Too expensive."),
-      Some(users.head.id),
-      Some(ProductInternal("2"))
-    ),
-    ReviewInternal(
-      UUID.fromString("add48364-a071-49a9-9b67-db95a22ba99a"),
-      Some("Could be better."),
-      Some(users(1).id),
-      Some(ProductInternal("3"))
-    ),
-    ReviewInternal(
-      UUID.fromString("aa5381ef-1531-4d6a-af09-6c8bd0c49843"),
-      Some("Prefer something else."),
-      Some(users(1).id),
-      Some(ProductInternal("1"))
+  val users = new AtomicReference(
+    Seq(
+      UserInternal(UUID.fromString("da02636a-a240-4607-bf63-85b4f76ec6f1"), Some("@ada")),
+      UserInternal(UUID.fromString("ad8f41df-c5d2-4a46-91a7-f637f9a08060"), Some("@complete"))
     )
   )
 
-  def getReviewsForUser(userId: UUID): Seq[Review] = reviews.filter(_.authorID.exists(_ == userId)).map { r =>
+  val reviews = new AtomicReference(
+    Seq(
+      ReviewInternal(
+        UUID.fromString("a6c43de7-5eab-4c83-97bf-66b44cf533e8"),
+        Some("Love it!"),
+        Some(users.get().head.id),
+        Some(ProductInternal("1"))
+      ),
+      ReviewInternal(
+        UUID.fromString("e4e8f4af-0161-4dad-8d2c-6c713381503f"),
+        Some("Too expensive."),
+        Some(users.get().head.id),
+        Some(ProductInternal("2"))
+      ),
+      ReviewInternal(
+        UUID.fromString("add48364-a071-49a9-9b67-db95a22ba99a"),
+        Some("Could be better."),
+        Some(users.get()(1).id),
+        Some(ProductInternal("3"))
+      ),
+      ReviewInternal(
+        UUID.fromString("aa5381ef-1531-4d6a-af09-6c8bd0c49843"),
+        Some("Prefer something else."),
+        Some(users.get()(1).id),
+        Some(ProductInternal("1"))
+      )
+    )
+  )
+
+  def getReviewsForUser(userId: UUID): Seq[Review] = reviews.get().filter(_.authorID.exists(_ == userId)).map { r =>
     Review(r.id, r.body, userQuery(r.authorID), productQuery(r.product))
   }
 
-  def getReviewsForProduct(upc: String): Seq[Review] = reviews.filter(_.product.exists(_.upc == upc)).map { r =>
+  def getReviewsForProduct(upc: String): Seq[Review] = reviews.get().filter(_.product.exists(_.upc == upc)).map { r =>
     Review(r.id, r.body, userQuery(r.authorID), productQuery(r.product))
   }
 
@@ -84,6 +89,7 @@ object ReviewsApi {
     ZQuery.foreach(id)(id =>
       ZQuery.succeed(
         users
+          .get()
           .find(_.id == id)
           .map(u => User(u.id, u.username, ZQuery.succeed(getReviewsForUser(u.id))))
           .getOrElse(User(id, None, ZQuery.succeed(getReviewsForUser(id))))
@@ -92,18 +98,37 @@ object ReviewsApi {
   val productQuery: Option[ProductInternal] => ZQuery[Any, Nothing, Option[Product]] = (pi: Option[ProductInternal]) =>
     ZQuery.foreach(pi)(p => ZQuery.succeed(Product(p.upc, ZQuery.succeed(getReviewsForProduct(p.upc)))))
 
+  def getReview(args: ReviewArgs): ZQuery[Any, Nothing, Option[Review]] =
+    ZQuery.succeed(
+      reviews
+        .get()
+        .find(_.id == args.id)
+        .map { r => Review(r.id, r.body, userQuery(r.authorID), productQuery(r.product)) }
+    )
+
+  def addReview(args: AddReviewArgs): Unit = {
+    reviews.set(
+      reviews
+        .get() :+ ReviewInternal(args.reviewId, Some(args.body), Some(args.authorId), Some(ProductInternal(args.upc)))
+    )
+    users.set(users.get() :+ UserInternal(args.authorId, None))
+  }
+
   case class ReviewArgs(id: UUID)
   case class UserArgs(id: UUID)
   case class ProductArgs(upc: String)
+  case class AddReviewArgs(reviewId: UUID, body: String, authorId: UUID, upc: String)
 
-  val api = graphQL(RootResolver()) @@ federated(
-    EntityResolver.from[ReviewArgs](args =>
-      ZQuery.succeed(reviews.find(_.id == args.id).map { r =>
-        Review(r.id, r.body, userQuery(r.authorID), productQuery(r.product))
-      })
-    ),
+  case class Queries(getReview: ReviewArgs => ZQuery[Any, Nothing, Option[Review]])
+  case class Mutations(addReview: AddReviewArgs => Unit)
+
+  val queries: Queries     = Queries(getReview)
+  val mutations: Mutations = Mutations(addReview)
+
+  val api = graphQL(RootResolver(queries, mutations)) @@ federated(
+    EntityResolver.from[ReviewArgs](getReview),
     EntityResolver.from[UserArgs](args => userQuery(Some(args.id))),
     EntityResolver.from[ProductArgs](args => productQuery(Some(ProductInternal(args.upc))))
-  )  @@ ApolloFederatedTracing.wrapper
+  ) @@ ApolloFederatedTracing.wrapper
 
 }
